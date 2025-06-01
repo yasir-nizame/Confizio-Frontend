abb562237a99d5e4fa5fd91e36f4430b427d3d9b
yasir-nizame <ynizami72@gmail.com>
2025-06-01 22:07:31 +0500

    CI/CD try

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ import ConferenceProceedingsForm from "./pages/Organizer/ConferenceProceedingsFo
 import ProceedingsPreview from "./pages/Organizer/ProceedingsPreview.js";
 
 function App() {
-  console.log("Hello world jhuy");
+  console.log("Hello world uy");
   return (
     <>
       <Toaster
